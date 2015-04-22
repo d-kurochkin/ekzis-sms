9cf946a724be3254bbbb89396a8a5105c25d2afb
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-22 17:35:57 +0600

    Реализовано получение баланса со счета пользователя

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject ekzis-sms "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.xml "0.0.8"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [ekzis-sms.core]
   :main ekzis-sms.core)
--- src/ekzis_sms/core.clj
+++ src/ekzis_sms/core.clj
@@ -1,20 +1,42 @@
 (ns ekzis-sms.core
   (:gen-class))
 
+(require '[clojure.xml :as xml])
+(require '[clojure.zip :as zip])
 
 (def service-url "https://mobizon.kz/service")
-
-(def balance-method "/user/getownbalance")
-
-(def api-key "apiKey=XXXXXXXXXXXXXX")
-
+(def api-key "apiKey=XXX")
 (def out-format "output=xml")
-  
+(def getownbalance-method "/user/getownbalance")
+
 (def balance-request (str  
                        service-url 
-                       balance-method "?"
+                       getownbalance-method "?"
                        out-format "&"
                        api-key))
+(defn request-data
+  [req]
+  (:content (xml/parse req)))
+
+(defn get-tag-content 
+  [tag data]
+  (->>
+    (filter #(= tag (:tag %)) data)
+    first
+    :content))
+ 
+(defn get-balance
+  []
+  (->>
+    balance-request
+    request-data
+    (get-tag-content :data)
+    (get-tag-content :balance)
+    first
+    read-string))
+
+    
+    
 
 (defn -main
   []
