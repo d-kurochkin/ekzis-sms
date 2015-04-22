3ddcaf3239f8f1914055de1b61166d1a6731c77e
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-22 16:43:46 +0600

    Формирование запроса для получения баланса

--- src/ekzis_sms/core.clj
+++ src/ekzis_sms/core.clj
@@ -1,6 +1,21 @@
 (ns ekzis-sms.core
   (:gen-class))
 
+
+(def service-url "https://mobizon.kz/service")
+
+(def balance-method "/user/getownbalance")
+
+(def api-key "apiKey=XXXXXXXXXXXXXX")
+
+(def out-format "output=xml")
+  
+(def balance-request (str  
+                       service-url 
+                       balance-method "?"
+                       out-format "&"
+                       api-key))
+
 (defn -main
   []
-  (println "Hello, World!"))
+  (println balance-request))
