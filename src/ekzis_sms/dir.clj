8bc4211358d67091c5e7618f4821654927bbbc6d
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-23 14:52:13 +0600

    Добавлена функция для получения следующего файла в очереди отправки

--- src/ekzis_sms/dir.clj
+++ src/ekzis_sms/dir.clj
@@ -1,3 +1,17 @@
 (ns ekzis-sms.dir
   (:require [clojure.java.io :as io]))
 
+(def directory (io/file "/home/pengo/Development/ekzis-sms/data/"))
+
+(defn check-ekz
+  [file]
+  (.contains (str file) ".ekz"))
+
+(defn get-next-file
+  []
+  (->>
+    directory
+    file-seq
+    (filter check-ekz)
+    first))
+  
