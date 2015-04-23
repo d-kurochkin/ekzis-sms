d1e23a1c9599d8915ac23d75d072c72a98a76876
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-23 18:01:10 +0600

    Реализовано перемещение файла

--- src/ekzis_sms/sms/dir.clj
+++ src/ekzis_sms/sms/dir.clj
@@ -1,10 +1,12 @@
 (ns ekzis-sms.sms.dir
   (:use ekzis-sms.xml)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(import '(java.io File))
 
 (def input-directory (io/file "/home/pengo/Development/ekzis-sms/data/in"))
 (def output-directory (io/file "/home/pengo/Development/ekzis-sms/data/out"))
-
 
 (defn check-ekz
   [file]
@@ -17,9 +19,15 @@
     file-seq
     (filter check-ekz)
     first))
+
+(defn move-file
+  [file]
+  (let [file_path (str file)
+        file_name (last (str/split file_name #"/"))
+        out_path  (str output-directory "/" file_name)]
+    (.renameTo (File. file_path) (File. out_path))))
+    
   
-
-
 (defn parse-file
   [file]
   (let [data (get-xml-data file)
