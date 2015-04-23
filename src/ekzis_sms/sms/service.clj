07aa3267efbc5b1731806edde1d5932cec7be287
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-23 16:34:13 +0600

    Удалены неиспользумые подключения модулей

--- src/ekzis_sms/sms/service.clj
+++ src/ekzis_sms/sms/service.clj
@@ -1,6 +1,4 @@
-(ns ekzis-sms.sms.service
-  (:require [clojure.xml :as xml])
-  (:require [clojure.zip :as zip]))
+(ns ekzis-sms.sms.service)
 
 (def service-url "https://mobizon.kz/service")
 (def api-key "apiKey=0336f1db764f13bba0357f854da66fea9633233a")
@@ -9,14 +7,3 @@
 (def sendsmsmessage-method "/message/sendsmsmessage")
 
 (def url-encode #(. java.net.URLEncoder encode %))
-
-(defn request-data
-  [req]
-  (:content (xml/parse req)))
-
-(defn get-tag-content 
-  [tag data]
-  (->>
-    (filter #(= tag (:tag %)) data)
-    first
-    :content))
