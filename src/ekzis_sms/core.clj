f2046e6f53e330e126736b52f5efb82d1dfd6c49
Dmitriy Kurochkin <newpengo@mail.ru>
2015-04-24 13:30:46 +0600

    Реализован основной цикл рассылки смс

--- src/ekzis_sms/core.clj
+++ src/ekzis_sms/core.clj
@@ -4,12 +4,31 @@
         ekzis-sms.sms.dir)
   (:gen-class))
 
-(defn start-balance-task
+(defn main-task
+  []
+  (let 
+    [sms-file (get-next-file)]
+    (when sms-file
+      (send-message sms-file)
+      (->> (get-balance) (write-balance)))))
+  
+
+(defn main-task-loop
+  []
+  (loop
+    (try
+     (main-task)
+     (catch Exception e (str "Exception: " (.getMessage e))))
+    (Thread/sleep 5000) 
+    (recur)))
+   
+(defn start-main-task
   []
   (doto 
-   (Thread. check-balance-loop)
+   (Thread. main-task-loop)
    ;(.setDaemon true)
    (.start)))
+
 
 
 
